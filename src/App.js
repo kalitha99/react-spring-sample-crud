c9957510ac2c60b03e9abbfd8c644fe97d3ebe00
Kalitha <kalithadissanayake44@gmail.com>
2022-03-08 15:55:28 +0530

    add and view student

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Appbar from "./components/Appbar";
+import ShowStudent from "./components/ShowStudent";
+import Student from "./components/Student";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Appbar />
+            <Student />
+          </Route>
+
+          <Route exact path="/info">
+            <Appbar />
+            <ShowStudent />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Student.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { Container, Paper, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function Student() {
+  const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [students, setStudents] = useState([]);
+  const classes = useStyles();
+  const [load, setload] = useState(true);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New Student added");
+    });
+    setload(!load);
+    setName("");
+    setAddress("");
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then((res) => res.json())
+      .then((result) => {
+        setStudents(result);
+      });
+  }, [load]);
+
+  const handelDelete = (id) => {
+    fetch(`http://localhost:8080/student/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Deleted");
+    });
+    setload(!load);
+  };
+
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: "blue" }}>Add Student</h1>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Student Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Student Adress"
+            variant="outlined"
+            fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <Button variant="contained" color="secondary" onClick={handleClick}>
+            Submit
+          </Button>
+        </form>
+      </Paper>
+      {/*
+      {name}
+      {address}
+  */}
+      <h1>Students</h1>
+
+      <Paper elevation={3} style={paperStyle}>
+        {students.map((student, index) => (
+          <Paper
+            elevation={6}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}
+          >
+            <Link
+              to={{
+                pathname: "/info",
+                state: { id: student.id },
+              }}
+            >
+              Id:{student.id}
+              <br />
+              Name:{student.name}
+              <br />
+              Address:{student.address}
+            </Link>
+            <Button onClick={() => handelDelete(student.id)}>DELETE</Button>
+          </Paper>
+        ))}
+      </Paper>
+    </Container>
+  );
+}
